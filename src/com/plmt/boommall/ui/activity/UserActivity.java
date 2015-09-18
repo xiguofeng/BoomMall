da6a768fab4d22a365692e6ee6473b66836f21b4
xgf <king.xgf@gmail.com>
2015-09-18 17:29:31 +0800

    add imagecorp logic

--- src/com/plmt/boommall/ui/activity/UserActivity.java
+++ src/com/plmt/boommall/ui/activity/UserActivity.java
@@ -2,13 +2,20 @@ package com.plmt.boommall.ui.activity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.plmt.boommall.R;
+import com.plmt.boommall.utils.cropimage.ChooseDialog;
+import com.plmt.boommall.utils.cropimage.CropHelper;
+import com.plmt.boommall.utils.cropimage.uitls.OSUtils;
 
 public class UserActivity extends Activity implements OnClickListener {
 
@@ -16,6 +23,9 @@ public class UserActivity extends Activity implements OnClickListener {
 
 	private TextView mUserNameTv;
 
+	private CropHelper mCropHelper;
+	private ChooseDialog mDialog;
+	private ImageView headImage;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,11 @@ public class UserActivity extends Activity implements OnClickListener {
 	}
 
 	private void initView() {
+		headImage = (ImageView) findViewById(R.id.user_icon_iv);
+		mCropHelper = new CropHelper(this, OSUtils.getSdCardDirectory()
+				+ "/head.png");
+		mDialog = new ChooseDialog(this, mCropHelper);
+		headImage.setOnClickListener(this);
 
 	}
 
@@ -41,7 +56,37 @@ public class UserActivity extends Activity implements OnClickListener {
 	}
 
 	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.e("onActivityResult", requestCode + "**" + resultCode);
+		if (requestCode == RESULT_CANCELED) {
+			return;
+		} else {
+			switch (requestCode) {
+			case CropHelper.HEAD_FROM_ALBUM:
+				mCropHelper.getDataFromAlbum(data);
+				Log.e("onActivityResult", "接收到图库图片");
+				break;
+			case CropHelper.HEAD_FROM_CAMERA:
+				mCropHelper.getDataFromCamera(data);
+				Log.e("onActivityResult", "接收到拍照图片");
+				break;
+			case CropHelper.HEAD_SAVE_PHOTO:
+				if (data != null && data.getParcelableExtra("data") != null) {
+					headImage.setImageBitmap((Bitmap) data
+							.getParcelableExtra("data"));
+					mCropHelper.savePhoto(data, OSUtils.getSdCardDirectory()
+							+ "/myHead.png");
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	@Override
 	public void onClick(View v) {
+		mDialog.popSelectDialog();
 	}
 
 	@Override
