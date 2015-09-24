dbd0211b74c7bdf380039bf61b471eb72dfae5e4
xgf <king.xgf@gmail.com>
2015-09-24 14:16:05 +0800

    add:pagingGirdView

---
+++ src/com/plmt/boommall/ui/view/gridview/paging/LoadingView.java
@@ -0,0 +1,25 @@
+package com.plmt.boommall.ui.view.gridview.paging;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import com.plmt.boommall.R;
+
+public class LoadingView extends LinearLayout {
+
+	public LoadingView(Context context) {
+		super(context);
+		init();
+	}
+
+	public LoadingView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	private void init() {
+		inflate(getContext(), R.layout.gv_paging_loading_view, this);
+	}
+
+}
