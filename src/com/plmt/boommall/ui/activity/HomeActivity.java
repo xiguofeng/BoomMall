15ef511b80f51517c40a493c9f8dc3b9f6b1f592
xgf <xiguofeng@163.com>
2015-10-05 11:46:34 +0800

    modify

--- src/com/plmt/boommall/ui/activity/HomeActivity.java
+++ src/com/plmt/boommall/ui/activity/HomeActivity.java
@@ -31,7 +31,7 @@ public class HomeActivity extends TabActivity implements OnClickListener {
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.home_new);
+		setContentView(R.layout.home);
 		initView();
 		initData();
 	}
