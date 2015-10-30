e9991fa26544035ba96fa22eb17d2e4a80416dda
xgf <king.xgf@gmail.com>
2015-10-30 14:04:12 +0800

    update:shopping cart

--- src/com/plmt/boommall/ui/activity/ShoppingCartActivity.java
+++ src/com/plmt/boommall/ui/activity/ShoppingCartActivity.java
@@ -18,55 +18,55 @@ import com.plmt.boommall.entity.GroupBean;
 import com.plmt.boommall.ui.adapter.ShoppingCartAdapter;
 import com.plmt.boommall.ui.adapter.ShoppingCartAdapter.ischeck;
 
-public class ShoppingCartActivity extends Activity implements ischeck{
+public class ShoppingCartActivity extends Activity implements ischeck {
 
 	private ExpandableListView list;
 	private ShoppingCartAdapter adapter;
-	private List<GroupBean> grou;
-	private Map<String, List<GroupBean>> chile;
+	private List<GroupBean> mGroup;
+	private Map<String, List<GroupBean>> mChild;
 	private GroupBean bean;
 	private CheckBox cb;
 	private LinearLayout empty;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.shopping_cart_expandablelistview);
-		this.list = (ExpandableListView)findViewById(R.id.lv);
+		this.list = (ExpandableListView) findViewById(R.id.lv);
 		empty = (LinearLayout) findViewById(R.id.empty);
 		cb = (CheckBox) findViewById(R.id.cb);
-		grou = new ArrayList<GroupBean>();
+		mGroup = new ArrayList<GroupBean>();
 		bean = new GroupBean();
 		bean.setTxt("一个商家");
-		grou.add(bean);
+		mGroup.add(bean);
 		bean = new GroupBean();
 		bean.setTxt("另一个供货商");
-		grou.add(bean);
+		mGroup.add(bean);
 		bean = new GroupBean();
 		bean.setTxt("最后一个");
-		grou.add(bean);
+		mGroup.add(bean);
 		bean = new GroupBean();
 		bean.setTxt("另一个供货商123");
-		grou.add(bean);
+		mGroup.add(bean);
 		bean = new GroupBean();
 		bean.setTxt("另一个供货商098");
-		grou.add(bean);
-		
-		this.chile = new HashMap<String,List<GroupBean>>();
+		mGroup.add(bean);
+
+		this.mChild = new HashMap<String, List<GroupBean>>();
 		List<GroupBean> chiless = new ArrayList<GroupBean>();
 		bean = new GroupBean();
 		bean.setTxt("好商品");
 		chiless.add(bean);
 		bean.setTxt("又好商品");
 		chiless.add(bean);
-		
-		chile.put(grou.get(0).getTxt(), chiless);
+
+		mChild.put(mGroup.get(0).getTxt(), chiless);
 		chiless = new ArrayList<GroupBean>();
 		bean = new GroupBean();
 		bean.setTxt("就一个");
 		chiless.add(bean);
-		
-		chile.put(grou.get(1).getTxt(), chiless);
+
+		mChild.put(mGroup.get(1).getTxt(), chiless);
 		chiless = new ArrayList<GroupBean>();
 		bean = new GroupBean();
 		bean.setTxt("333好商品");
@@ -74,15 +74,15 @@ public class ShoppingCartActivity extends Activity implements ischeck{
 		bean = new GroupBean();
 		bean.setTxt("1111又好商品");
 		chiless.add(bean);
-		
+
 		bean = new GroupBean();
 		bean.setTxt("1115又好商品");
 		chiless.add(bean);
 		bean = new GroupBean();
 		bean.setTxt("11177又好商品");
 		chiless.add(bean);
-		
-		chile.put(grou.get(2).getTxt(), chiless);
+
+		mChild.put(mGroup.get(2).getTxt(), chiless);
 		chiless = new ArrayList<GroupBean>();
 		bean = new GroupBean();
 		bean.setTxt("333好商品");
@@ -99,10 +99,8 @@ public class ShoppingCartActivity extends Activity implements ischeck{
 		bean = new GroupBean();
 		bean.setTxt("111777好商品");
 		chiless.add(bean);
-		
-		
-		
-		chile.put(grou.get(3).getTxt(), chiless);
+
+		mChild.put(mGroup.get(3).getTxt(), chiless);
 		chiless = new ArrayList<GroupBean>();
 		bean = new GroupBean();
 		bean.setTxt("333好商品");
@@ -119,26 +117,24 @@ public class ShoppingCartActivity extends Activity implements ischeck{
 		bean = new GroupBean();
 		bean.setTxt("111777好商品");
 		chiless.add(bean);
-		
-		chile.put(grou.get(4).getTxt(), chiless);
-		adapter = new ShoppingCartAdapter(this, grou, chile);
+
+		mChild.put(mGroup.get(4).getTxt(), chiless);
+		adapter = new ShoppingCartAdapter(this, mGroup, mChild);
 		adapter.setischek(this);
-		
-		
+
 		list.setAdapter(adapter);
 		int size = adapter.getGroupCount();
-		for(int i = 0;i<size;i++){
+		for (int i = 0; i < size; i++) {
 			list.expandGroup(i);
 		}
 		cb.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
-				int size = grou.size();
-				for(int i = 0;i<size;i++){
-					grou.get(i).setIscheck(cb.isChecked());
+
+				int size = mGroup.size();
+				for (int i = 0; i < size; i++) {
+					mGroup.get(i).setIscheck(cb.isChecked());
 					ischekgroup(i, cb.isChecked());
 				}
 				adapter.notifyDataSetChanged();
@@ -146,17 +142,16 @@ public class ShoppingCartActivity extends Activity implements ischeck{
 		});
 		list.setEmptyView(empty);
 	}
+
 	@Override
 	public void ischekgroup(int groupposition, boolean ischeck) {
-		// TODO Auto-generated method stub
-		List<GroupBean> chiless = chile.get(grou.get(groupposition).getTxt());
-		for(GroupBean bean:chiless){
+		List<GroupBean> child = mChild
+				.get(mGroup.get(groupposition).getTxt());
+		for (GroupBean bean : child) {
 			bean.setIscheck(ischeck);
 		}
-		grou.get(groupposition).setIscheck(ischeck);
+		mGroup.get(groupposition).setIscheck(ischeck);
 		adapter.notifyDataSetChanged();
 	}
-	
-
 
 }
--- src/com/plmt/boommall/ui/adapter/ShoppingCartAdapter.java
+++ src/com/plmt/boommall/ui/adapter/ShoppingCartAdapter.java
@@ -20,13 +20,16 @@ import com.plmt.boommall.entity.GroupBean;
 
 public class ShoppingCartAdapter extends BaseExpandableListAdapter {
 	List<GroupBean> grouds;
-	Map<String,List<GroupBean>> chiles;
+	Map<String, List<GroupBean>> chiles;
 	Context contenx;
 	ischeck ischeck;
-	public void setischek(ischeck ischeck){
+
+	public void setischek(ischeck ischeck) {
 		this.ischeck = ischeck;
 	}
-	public ShoppingCartAdapter(Context context,List<GroupBean> grouds,Map<String,List<GroupBean>> chiles){
+
+	public ShoppingCartAdapter(Context context, List<GroupBean> grouds,
+			Map<String, List<GroupBean>> chiles) {
 		this.contenx = context;
 		this.grouds = grouds;
 		this.chiles = chiles;
@@ -34,53 +37,48 @@ public class ShoppingCartAdapter extends BaseExpandableListAdapter {
 
 	@Override
 	public int getGroupCount() {
-		// TODO Auto-generated method stub
 		return grouds.size();
 	}
 
 	@Override
 	public int getChildrenCount(int groupPosition) {
-		// TODO Auto-generated method stub
-		return ((List<GroupBean>)chiles.get(grouds.get(groupPosition).getTxt())).size();
+		return ((List<GroupBean>) chiles
+				.get(grouds.get(groupPosition).getTxt())).size();
 	}
 
 	@Override
 	public Object getGroup(int groupPosition) {
-		// TODO Auto-generated method stub
 		return grouds.get(groupPosition);
 	}
 
 	@Override
 	public Object getChild(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
-		return chiles.get(grouds.get(groupPosition).getTxt()).get(childPosition);
+		return chiles.get(grouds.get(groupPosition).getTxt())
+				.get(childPosition);
 	}
 
 	@Override
 	public long getGroupId(int groupPosition) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public long getChildId(int groupPosition, int childPosition) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public boolean hasStableIds() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	public View getGroupView(final int groupPosition, boolean isExpanded,
 			View convertView, ViewGroup parent) {
-		// TODO Auto-generated method stub
 		ViewHolder holder;
-		if(convertView==null){
-			convertView = LayoutInflater.from(contenx).inflate(R.layout.shoping_cart_group, null);
+		if (convertView == null) {
+			convertView = LayoutInflater.from(contenx).inflate(
+					R.layout.shoping_cart_group, null);
 			holder = new ViewHolder();
 			convertView.setTag(holder);
 			holder.tv = (TextView) convertView.findViewById(R.id.tv);
@@ -88,45 +86,48 @@ public class ShoppingCartAdapter extends BaseExpandableListAdapter {
 		}
 		holder = (ViewHolder) convertView.getTag();
 		holder.tv.setText(grouds.get(groupPosition).getTxt());
-		if(grouds.get(groupPosition).isIscheck()){
+		if (grouds.get(groupPosition).isIscheck()) {
 			holder.btn.setChecked(true);
-		}else{
+		} else {
 			holder.btn.setChecked(false);
 		}
 		holder.tv.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Toast.makeText(contenx, ((TextView)v).getText().toString(), Toast.LENGTH_SHORT).show();
+				Toast.makeText(contenx, ((TextView) v).getText().toString(),
+						Toast.LENGTH_SHORT).show();
 			}
 		});
 		holder.btn.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			
+
 			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-				// TODO Auto-generated method stub
-				if(isChecked){
-					
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				if (isChecked) {
+
 				}
 			}
 		});
 		holder.btn.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				if(((CheckBox)v).isChecked()){
+				if (((CheckBox) v).isChecked()) {
 					grouds.get(groupPosition).setIscheck(true);
-					Toast.makeText(contenx, groupPosition+"ischeck"+true, Toast.LENGTH_SHORT).show();
-					
+					Toast.makeText(contenx, groupPosition + "ischeck" + true,
+							Toast.LENGTH_SHORT).show();
+
 					ischeck.ischekgroup(groupPosition, true);
-				}else{
-//					grouds.get(groupPosition).setIscheck(false);
-//					List<GroupBean> chiless = chiles.get(grouds.get(groupPosition).getTxt());
-//					for(GroupBean bean:chiless){
-//						bean.setIscheck(false);
-//					}
+				} else {
+					// grouds.get(groupPosition).setIscheck(false);
+					// List<GroupBean> chiless =
+					// chiles.get(grouds.get(groupPosition).getTxt());
+					// for(GroupBean bean:chiless){
+					// bean.setIscheck(false);
+					// }
 					ischeck.ischekgroup(groupPosition, false);
 				}
 			}
@@ -138,31 +139,36 @@ public class ShoppingCartAdapter extends BaseExpandableListAdapter {
 	public View getChildView(final int groupPosition, final int childPosition,
 			boolean isLastChild, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
-		ViewHolderChile viewholder;
-		if(convertView==null){
-			convertView = LayoutInflater.from(contenx).inflate(R.layout.shoping_cart_item, null);
-			viewholder = new ViewHolderChile();
+		ViewHolderChild viewholder;
+		if (convertView == null) {
+			convertView = LayoutInflater.from(contenx).inflate(
+					R.layout.shoping_cart_item, null);
+			viewholder = new ViewHolderChild();
 			convertView.setTag(viewholder);
-			viewholder.tv = (CheckBox)convertView.findViewById(R.id.tv);
+			viewholder.tv = (CheckBox) convertView.findViewById(R.id.tv);
 			viewholder.btn = (TextView) convertView.findViewById(R.id.tv2);
 		}
-		viewholder = (ViewHolderChile)convertView.getTag();
-		viewholder.tv.setText( this.chiles.get(grouds.get(groupPosition).getTxt()).get(childPosition).getTxt());
-		viewholder.btn.setText(viewholder.tv.getText()+"商品描述");
-		if(this.chiles.get(grouds.get(groupPosition).getTxt()).get(childPosition).isIscheck()){
+		viewholder = (ViewHolderChild) convertView.getTag();
+		viewholder.tv.setText(this.chiles
+				.get(grouds.get(groupPosition).getTxt()).get(childPosition)
+				.getTxt());
+		viewholder.btn.setText(viewholder.tv.getText() + "商品描述");
+		if (this.chiles.get(grouds.get(groupPosition).getTxt())
+				.get(childPosition).isIscheck()) {
 			viewholder.tv.setChecked(true);
-		}else{
+		} else {
 			viewholder.tv.setChecked(false);
 		}
 		viewholder.tv.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				if(((CheckBox)v).isChecked()){
-					chiles.get(grouds.get(groupPosition).getTxt()).get(childPosition).setIscheck(true);
-				}else{
-					chiles.get(grouds.get(groupPosition).getTxt()).get(childPosition).setIscheck(false);
+				if (((CheckBox) v).isChecked()) {
+					chiles.get(grouds.get(groupPosition).getTxt())
+							.get(childPosition).setIscheck(true);
+				} else {
+					chiles.get(grouds.get(groupPosition).getTxt())
+							.get(childPosition).setIscheck(false);
 				}
 			}
 		});
@@ -174,17 +180,19 @@ public class ShoppingCartAdapter extends BaseExpandableListAdapter {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
-	public static class ViewHolder{
+
+	public static class ViewHolder {
 		private CheckBox btn;
 		private TextView tv;
 	}
-	public static class ViewHolderChile{
+
+	public static class ViewHolderChild {
 		private TextView btn;
 		private CheckBox tv;
 	}
-	public interface ischeck{
-		public void ischekgroup(int groupposition,boolean ischeck)
-;	}
+
+	public interface ischeck {
+		public void ischekgroup(int groupposition, boolean ischeck);
+	}
 
 }
