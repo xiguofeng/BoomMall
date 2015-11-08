7b7f3760a88b7b11180db7b93f2654d6d0c36817
xgf <xiguofeng@163.com>
2015-11-08 21:26:54 +0800

    add

---
+++ src/com/plmt/boommall/entity/SubmitOrderResponse.java
@@ -0,0 +1,25 @@
+package com.plmt.boommall.entity;
+
+public class SubmitOrderResponse {
+	
+	private String order_id;
+
+	private String order_status;
+
+	public String getOrder_id() {
+		return order_id;
+	}
+
+	public void setOrder_id(String order_id) {
+		this.order_id = order_id;
+	}
+
+	public String getOrder_status() {
+		return order_status;
+	}
+
+	public void setOrder_status(String order_status) {
+		this.order_status = order_status;
+	}
+	
+}
